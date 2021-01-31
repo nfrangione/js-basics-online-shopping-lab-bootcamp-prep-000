e6480c51d24e4dd0c611cc3bba4523f351e25ff8
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-31 01:33:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -82,11 +82,13 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  var orderMessage = "";
   if (cardNumber === undefined) {
-    console.log("Sorry, we don't have a credit card on file for you.");
+    orderMessage = "Sorry, we don't have a credit card on file for you.";
   }
   else {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    orderMessage = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
   cart = [];
+  return orderMessage;
 }
