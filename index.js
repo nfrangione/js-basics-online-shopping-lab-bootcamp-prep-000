5f9dd351aaf1e1cb433b0f81d87ec96c1d2f0ea1
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-31 01:30:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -82,9 +82,6 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
-  var itemWithPrice;
-  var price = [];
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   }
