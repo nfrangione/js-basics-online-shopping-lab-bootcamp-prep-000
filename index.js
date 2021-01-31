b21a0c6917869017533ed2ec58ba8a48334ee663
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-31 01:22:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -85,5 +85,10 @@ function placeOrder(cardNumber) {
   // write your code here
   var itemWithPrice;
   var price = [];
-  
+  if (cardNumber === null) {
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  else {
+    
+  }
 }
